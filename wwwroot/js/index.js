23b8b81585845ff8942c8928ee08b287bc657530
mikelisrael <israelipinkz@gmail.com>
2023-01-03 18:21:22 +0100

    Progress on home page's hero

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -4,17 +4,22 @@
 // Show navbar on scroll up and hide on scroll down
 let lastScrollTop = 0;
 const navbar = document.getElementById("navbar");
+const logo = document.getElementById("logo");
+
 window.addEventListener("scroll", function () {
-  navbar.classList.add("activate_scroll");
   const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
   if (scrollTop == 0) {
-    // at top, remove background color
+    // at top, remove background color & change logo
+    logo.setAttribute("src", "/images/logo-light.png");
+    navbar.classList.remove("activate_scroll");
     navbar.style.backgroundColor = "transparent";
   } else if (scrollTop > lastScrollTop) {
     // Scroll down
+    navbar.classList.add("activate_scroll");
     navbar.style.top = "-120px";
   } else {
     // Scroll up
+    logo.setAttribute("src", "/images/logo-dark.png");
     navbar.style.top = "0";
     navbar.style.backgroundColor = "white";
   }
