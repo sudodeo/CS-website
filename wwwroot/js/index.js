cc35541f4f0bba98bf1802286d87507ca79063cc
mikelisrael <israelipinkz@gmail.com>
2023-01-03 15:58:08 +0100

    Navbar show/hide on scroll

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -1,4 +1,22 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+// Show navbar on scroll up and hide on scroll down
+let lastScrollTop = 0;
+const navbar = document.getElementById("navbar");
+window.addEventListener("scroll", function () {
+  navbar.classList.add("activate_scroll");
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrollTop == 0) {
+    // at top, remove background color
+    navbar.style.backgroundColor = "transparent";
+  } else if (scrollTop > lastScrollTop) {
+    // Scroll down
+    navbar.style.top = "-120px";
+  } else {
+    // Scroll up
+    navbar.style.top = "0";
+    navbar.style.backgroundColor = "white";
+  }
+  lastScrollTop = scrollTop;
+});
