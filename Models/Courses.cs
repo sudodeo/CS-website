459912a1bcf9d8817bd4dfe5a5fddf86e7aff7a5
sudodeo <adesolaadeoluwa@gmail.com>
2023-03-01 02:23:23 +0100

    Rename model to plural

--- Models/Courses.cs
+++ Models/Courses.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 using Microsoft.EntityFrameworkCore;
@@ -7,20 +9,15 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CS_website.Models
 {
+    [Table("courses", Schema = "public")]
     public class Course
     {
+        [Key]
         public string CourseCode { get; set; }
         public string CourseTitle { get; set; }
         public string CourseDescription { get; set; }
         public string CourseStatus { get; set; }
+        public string Level { get; set; }
     }
 
-    public class CourseContext: DbContext
-    {
-        public CourseContext(DbContextOptions<CourseContext> options) : base(options)
-        {
-        }
-
-        public DbSet<Course> Courses { get; set; }
-    }
 }
