4eac2231fbc27792e1d0c9644784670e1aec8382
sudodeo <adesolaadeoluwa@gmail.com>
2023-03-01 08:40:11 +0100

    Add database migrations

---
+++ Migrations/20230301025452_initCSC.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CS_website.Migrations
+{
+    /// <inheritdoc />
+    public partial class initCSC : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "public");
+
+            migrationBuilder.CreateTable(
+                name: "courses",
+                schema: "public",
+                columns: table => new
+                {
+                    code = table.Column<string>(type: "text", nullable: false),
+                    level = table.Column<string>(type: "text", nullable: false),
+                    title = table.Column<string>(type: "text", nullable: false),
+                    description = table.Column<string>(type: "text", nullable: false),
+                    status = table.Column<string>(type: "text", nullable: false),
+                    units = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_courses", x => x.code);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "courses",
+                schema: "public");
+        }
+    }
+}
