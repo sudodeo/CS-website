9ea477694e4e5e829537284f69b861d81c3d60bf
sudodeo <adesolaadeoluwa@gmail.com>
2023-03-01 08:39:49 +0100

    Add db context and seeder for database

--- Data/CourseContext.cs
+++ Data/CourseContext.cs
@@ -4,21 +4,36 @@ using System.Linq;
 using System.Threading.Tasks;
 using CS_website.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace CS_website.Data
 {
-        public class CourseContext: DbContext
+    public class CourseContext : DbContext
     {
+        public CourseContext()
+        {
+        }
+
         public CourseContext(DbContextOptions<CourseContext> options) : base(options)
         {
         }
 
         public DbSet<Course> Course { get; set; }
+
+        public void Seed()
+        {
+            using (var reader = new StreamReader("Scripts/PopulateDB.sql"))
+            {
+                string sql = reader.ReadToEnd();
+                Database.ExecuteSqlRaw(sql);
+            }
+        }
+        // protected override void OnConfiguring(DbContextOptionsBuilder options)
+        // {
+        //     // connect to postgres with connection string from app settings
+        //     options.UseNpgsql(Configuration.GetConnectionString("CourseLibraryDB"));
+        // }
     }
-    
-    // protected override void OnConfiguring(DbContextOptionsBuilder options)
-    // {
-    //     // connect to postgres with connection string from app settings
-    //     options.UseNpgsql(Configuration.GetConnectionString("CourseLibraryDB"));
-    // }
+
+
 }
