59bfcc96abb6af3ea6410e74094971a5525fbe55
sudodeo <adesolaadeoluwa@gmail.com>
2023-03-01 08:42:28 +0100

    Add dynamic rendering

--- Controllers/CurriculumController.cs
+++ Controllers/CurriculumController.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using CS_website.Models;
+using CS_website.Data;
 
 namespace CS_website.Controllers;
 
@@ -8,15 +9,29 @@ public class CurriculumController : Controller
 {
     private readonly ILogger<CurriculumController> _logger;
 
-    public CurriculumController(ILogger<CurriculumController> logger)
+    private readonly CourseContext _context;
+
+    public CurriculumController(ILogger<CurriculumController> logger, CourseContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
-    public IActionResult Index()
-    {
-        return View();
+    public IActionResult Index(int? level)
+    {          // Get all courses from the database
+        var courses = _context.Course.OrderBy(c => c.code).ToList();
+        // Console.WriteLine(level);
+        // // Filter courses by year if year parameter is specified
+        if (level.HasValue)
+        {
+            courses = courses.Where(c => c.level == level.ToString()).ToList();
+        }
+
+        // Pass the courses to the view
+        return View(courses);
+
     }
+
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
